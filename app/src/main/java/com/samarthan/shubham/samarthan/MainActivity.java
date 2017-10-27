073311eb1c2b2bdefc00c33811ace2441859341b
SINGHAM\SHUBHAM <singhams.shubham571@gmail.com>
2017-10-28 02:01:45 +0530

    Add scroll View and navigation drawer new activity

--- app/src/main/java/com/samarthan/shubham/samarthan/MainActivity.java
+++ app/src/main/java/com/samarthan/shubham/samarthan/MainActivity.java
@@ -1,7 +1,12 @@
 package com.samarthan.shubham.samarthan;
 
+import android.content.Intent;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void
+    public void jobSeekerLogin(View view){
+        Intent intent = new Intent(MainActivity.this,loginActivity.class);
+        intent.putExtra("personStatus",0);
+        startActivity(intent);
+    }
+
+    public void employerLogin(View view){
+        Intent intent = new Intent(MainActivity.this,loginActivity.class);
+        intent.putExtra("personStatus",1);
+        startActivity(intent);
+    }
+
+
 }
