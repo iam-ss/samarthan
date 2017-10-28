a407cae396f17e00c7ff1b7e90e306f2a6b1e6e6
SINGHAM\SHUBHAM <singhams.shubham571@gmail.com>
2017-10-28 12:01:25 +0530

    Added intents for passing employer information to the detail page. Added spinner details for disability dropdown menu. Removed debgger toast instances from the login activity page. Also set the edit text inpu type as password.

--- app/src/main/java/com/samarthan/shubham/samarthan/Employer.java
+++ app/src/main/java/com/samarthan/shubham/samarthan/Employer.java
@@ -1,15 +1,33 @@
 package com.samarthan.shubham.samarthan;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * Created by SHUBHAM on 28-10-2017.
  */
 
-public class Employer {
+public class Employer implements Parcelable{
     private String name;
     private ArrayList<String> joblist;
     private String phone;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public Employer(){
+
+    }
+
+    public Employer(Parcel in) {
+        name = in.readString();
+        joblist = in.createStringArrayList();
+        phone = in.readString();
+    }
 
     public String getEmail() {
         return email;
@@ -53,4 +71,22 @@ public class Employer {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(email);
+        parcel.writeStringList(joblist);
+        parcel.writeString(phone);
+    }
+
+    public static final Parcelable.Creator<Employer> CREATOR= new Parcelable.Creator<Employer>(){
+        public Employer createFromParcel(Parcel in) {
+            return new Employer(in);
+        }
+
+        public Employer[] newArray(int size) {
+            return new Employer[size];
+        }
+    };
 }
+
